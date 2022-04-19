02ebdfff7dcff0489fc3f261cb036a55efdce5e8
colinplante <91347736+colinplante@users.noreply.github.com>
2022-04-19 09:24:40 -0600

    Update ControlSystemMain.py

--- ControlSystemMain.py
+++ ControlSystemMain.py
@@ -10,6 +10,8 @@ import RPi.GPIO as GPIO
 import time
 import serial
 import schedule
+from tkinter import *
+from tkinter import ttk
 
 # start serial port and make it global
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=5)
@@ -19,6 +21,121 @@ FAN_PIN = 7
 DRIP_BOARD_PIN = 5
 WATER_PUMP_PIN = 3
 AUX_TANK_PUMP_PIN = 11
+global maxtempslider
+global mintempslider
+global maxhumslider
+global minhumslider
+global max_temp
+global min_temp
+global max_hum
+global min_hum
+
+def temp_win():
+   temp = Toplevel(win)
+   temp.geometry("750x250")
+   temp.title("Temperature")
+   #Create a Label in New window
+   #Label(new, text="What would you like the minimum temperature to be?", font=('Helvetica 17 bold')).pack(pady=30)
+
+   Label(temp, text="What would you like the maximum temperature to be?", font=('Helvetica 17 bold')).pack(pady=30)
+   ttk.Button(temp, text="Change Max Temp", command=max_temp_slider).pack()
+   Label(temp, text="What would you like the minimum temperature to be?", font=('Helvetica 17 bold')).pack(pady=30)
+   ttk.Button(temp, text="Change Min Temp", command=min_temp_slider).pack()
+
+
+def max_temp_slider():
+   global maxtempslider
+   temps = Toplevel(win)
+   win.geometry('750x250')
+   win.title('Temperature Slider')
+   maxtempslider = Scale(temps, from_=0, to=150)
+   maxtempslider.pack()
+   max_temp = maxtempslider.get()
+   ttk.Button(temps, text = "Confirm new Temp", command = set_max_temp).pack()
+   max_temp = maxtempslider.get()
+   maxtempslider.set(max_temp)
+
+
+def set_max_temp():
+   max_temp = maxtempslider.get()
+   #maxtempslider.set(maxtemp)
+   print(maxtempslider.get())
+   print("New max temp is: ", max_temp)
+   maxtempslider.destroy()
+   win(Toplevel).destroy()
+
+def min_temp_slider():
+   temps = Toplevel(win)
+   win.geometry('750x250')
+   win.title('Temperature Slider')
+   mintempslider = Scale(temps, from_=0, to=150)
+   mintempslider.pack()
+   ttk.Button(temps, text = "Confirm new Temp", command = set_min_temp).pack()
+   min_temp = mintempslider.get()
+   mintempslider.set(min_temp)
+   
+def set_min_temp():
+   min_temp = mintempslider.get()
+   #mintempslider.set(min_temp)
+   print('New min temp is: ', min_temp)
+   
+def hum_win():
+   hum= Toplevel(win)
+   hum.geometry("750x250")
+   hum.title("Humidity")
+   #Create a Label in New window
+   Label(hum, text="What would you like the minimum humidity percentage to be?", font=('Helvetica 17 bold')).pack(pady=30)
+   ttk.Button(hum, text="Change Min Humidity Percentage", command=min_hum_slider).pack()
+   Label(hum, text="What would you like the maximum humidity percentage to be?", font=('Helvetica 17 bold')).pack(pady=30)
+   ttk.Button(hum, text="Change Max Humidity Percentage", command=max_hum_slider).pack()
+
+def min_hum_slider():
+   hums = Toplevel(win)
+   win.geometry('750x250')
+   win.title('Humidity Slider')
+   minhumslider = Scale(hums, from_=0, to=100)
+   minhumslider.pack()
+   ttk.Button(hums, text = "Confirm new humidity level", command = set_min_hum).pack()
+   min_hum = minhumslider.get()
+   minhumslider.set(min_hum)
+   
+def set_min_hum():
+   min_hum = minhumslider.get()
+   #mintempslider.set(min_temp)
+   print('New min temp is: ', min_hum)
+   
+def max_hum_slider():
+   hums = Toplevel(win)
+   win.geometry('750x250')
+   win.title('Humidity Slider')
+   maxhumslider = Scale(hums, from_=0, to=100)
+   maxhumslider.pack()
+   ttk.Button(hums, text = "Confirm new humidity level", command = set_max_hum).pack()
+   max_hum = max_hum_slider.get()
+
+def set_max_hum():
+   max_hum = maxhumslider.get()
+   #mintempslider.set(min_temp)
+   print('New max hum is: ', max_temp)
+   return max_temp
+   
+def soil_win():
+   soil= Toplevel(win)
+   soil.geometry("750x250")
+   soil.title("Soil")
+   #Create a Label in New window
+   Label(soil, text="Buncha soil stuff I have to figure out units for", font=('Helvetica 17 bold')).pack(pady=30)
+
+#Create a label
+Label(win, text= "Which subsystem would you like to view?", font= ('Helvetica 17 bold')).pack(pady=30)
+#Create a button to open a New Window
+ttk.Button(win, text="Temperature", command=temp_win).pack()
+ttk.Button(win, text="Humidity", command=hum_win).pack()
+ttk.Button(win, text="Soil Levels", command=soil_win).pack()
+
+
+win.mainloop()
+
 
 def DataRequest(request):
     # sending request
